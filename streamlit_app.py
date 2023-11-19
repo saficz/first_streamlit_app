d1d0e3e7772b1f47f4bf61db2c02079c7cba983a
saficz <151452977+saficz@users.noreply.github.com>
2023-11-19 22:21:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 
 streamlit.title("My Parents New Heathy Diner.")
-
-streamlit.title("Breakfast Menu.")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
